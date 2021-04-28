ee5c663520bd30e7e640fe0772a4c67882a18077
kensan914 <52157596+kensan914@users.noreply.github.com>
2021-04-28 16:27:00 +0900

    Initial Commit

---
+++ otapick-ios-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  otapick-ios-demo
+//
+//  Created by 鳥海健人 on 2021/04/28.
+//  Copyright © 2021 kentoToriumi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ otapick-ios-demoTests/otapick_ios_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  otapick_ios_demoTests.swift
+//  otapick-ios-demoTests
+//
+//  Created by 鳥海健人 on 2021/04/28.
+//  Copyright © 2021 kentoToriumi. All rights reserved.
+//
+
+import XCTest
+@testable import otapick_ios_demo
+
+class otapick_ios_demoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
